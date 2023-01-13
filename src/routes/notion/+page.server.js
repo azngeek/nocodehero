947a482ec9cf4e2785dc9d2bf16a876c7f11d7f5
donbosco <vanhoi@mothership.de>
2023-01-13 16:03:20 +0100

    Verwendung von Token

--- src/routes/notion/+page.server.js
+++ src/routes/notion/+page.server.js
@@ -1,14 +1,18 @@
 import { Client } from "@notionhq/client";
+import { NOTION_SECRET } from '$env/static/private';
+
+
 
 const notion = new Client({
-    auth: "secret_1K4m7fKCE6ZFcD3tEUCrxusoVfjqPyDOLFRG2wT3ukc",
+    auth: NOTION_SECRET
   })
+
+  console.log("ENV: " + NOTION_SECRET);
 
   const databaseId = '368bdbed87d74f95b213f9b3627f8bd3';
 
 export async function load() {
     const result = await notion.databases.query({database_id: databaseId});
-    console.log(result);
     return {
         "data": result
     }
